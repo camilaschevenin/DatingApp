87da79956ffb6e56796ff3225aaf7a76eac2d70f
Camila Schevenin <camila.schevenin@gmail.com>
2019-04-11 12:21:57 +1000

    master

---
+++ DatingApp-SPA/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { ValueComponent } from './value/value.component';
+import { TestComponent } from './test/test.component';
+import { NavComponent } from './nav/nav.component';
+
+@NgModule({
+   declarations: [
+      AppComponent,
+      ValueComponent,
+      TestComponent,
+      NavComponent
+   ],
+   imports: [
+      BrowserModule,
+      HttpClientModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
+})
+export class AppModule { }
